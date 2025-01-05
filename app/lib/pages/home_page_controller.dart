b88cf80d2356a349f01576a434aec06d51ad28ec
Lahiru Sampath <113624926+LahiruZaM@users.noreply.github.com>
2025-01-05 23:07:14 +0530

    Make some minor changes in home page controller section

--- app/lib/pages/home_page_controller.dart
+++ app/lib/pages/home_page_controller.dart
@@ -2,10 +2,11 @@ import 'package:fileflow/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:refena_flutter/refena_flutter.dart';
 
+// ViewModel for the Home Page
 class HomePageVm {
-  final PageController controller;
-  final HomeTab currentTab;
-  final void Function(HomeTab) changeTab;
+  final PageController controller; // Manages transitions between pages in the PageView
+  final HomeTab currentTab; // Represents the currently selected tab
+  final void Function(HomeTab) changeTab; // Function to handle tab changes
 
   HomePageVm({
     required this.controller,
@@ -14,29 +15,36 @@ class HomePageVm {
   });
 }
 
+// Provider for HomePageController, connecting the controller to the state management system
 final homePageControllerProvider = ReduxProvider<HomePageController, HomePageVm>(
   (ref) => HomePageController(),
 );
 
+// Controller for managing the logic and state of the Home Page
 class HomePageController extends ReduxNotifier<HomePageVm> {
   @override
   HomePageVm init() {
+    // Initialize the ViewModel with default values
     return HomePageVm(
-      controller: PageController(),
-      currentTab: HomeTab.receive,
-      changeTab: (tab) => redux.dispatch(ChangeTabAction(tab)),
+      controller: PageController(), // Initializes the PageController for page navigation
+      currentTab: HomeTab.receive, // Sets the default tab to 'receive'
+      changeTab: (tab) => redux.dispatch(ChangeTabAction(tab)), // Dispatches an action to change the tab
     );
   }
 }
 
+// Action for changing the current tab on the Home Page
 class ChangeTabAction extends ReduxAction<HomePageController, HomePageVm> {
-  final HomeTab tab;
+  final HomeTab tab; // The tab to switch to
 
   ChangeTabAction(this.tab);
 
   @override
   HomePageVm reduce() {
+    // Perform the tab change by updating the PageController's page index
     state.controller.jumpToPage(tab.index);
+
+    // Return a new ViewModel with the updated current tab
     return HomePageVm(
       controller: state.controller,
       currentTab: tab,
