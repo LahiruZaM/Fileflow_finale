0f9febfa653cc17cbf5503985882c7cb9618e8d7
Lahiru Sampath <113624926+LahiruZaM@users.noreply.github.com>
2025-01-07 17:25:13 +0530

    Send modes added

--- app/lib/model/send_mode.dart
+++ app/lib/model/send_mode.dart
@@ -1,5 +1,5 @@
 enum SendMode {
-  single, // 1:1 file transfer
-  multiple, // 1:n file transfer
-  link, // 1:n file transfer but receiver needs to initiate
+  single, // Mode for one-to-one file transfer where a single file is sent directly to one recipient
+  multiple, // Mode for one-to-many file transfer where the file is sent to multiple recipients
+  link, // Mode for one-to-many file transfer where the recipient initiates the download using a shared link
 }
