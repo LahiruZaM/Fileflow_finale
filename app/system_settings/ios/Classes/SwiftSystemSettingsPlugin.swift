bf9b0e7fdc4c63ed02f27a0cd7d556fdcf1f46d5
Akshayan <makshayan@std.appsc.sab.ac.lk>
2025-01-07 18:20:09 +0530

    Update Swift-System-Settings-Plugin

--- app/system_settings/ios/Classes/SwiftSystemSettingsPlugin.swift
+++ app/system_settings/ios/Classes/SwiftSystemSettingsPlugin.swift
@@ -2,21 +2,26 @@ import Flutter
 import UIKit
 
 public class SwiftSystemSettingsPlugin: NSObject, FlutterPlugin {
+  /// Registers the plugin with the Flutter plugin registrar.
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "system_settings", binaryMessenger: registrar.messenger())
     let instance = SwiftSystemSettingsPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
+  /// Handles incoming method calls from the Flutter side.
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-      openSettings()
+      openSettings() // Call the method to open the device settings.
   }
 
+  /// Opens the system settings of the iOS device.
   private func openSettings() {
-    if let url = URL(string: UIApplication.openSettingsURLString) {
+    if let url = URL(string: UIApplication.openSettingsURLString) { // URL for the system settings page.
         if #available(iOS 10.0, *) {
+            // For iOS 10.0 and above, use the open method with options.
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
         } else {
+            // For iOS versions below 10.0, use the openURL method.
             UIApplication.shared.openURL(url)
         }
     }
