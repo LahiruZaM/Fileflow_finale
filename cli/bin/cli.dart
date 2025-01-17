e0c420c99c57d5ed37dbcde76555eceb7600e209
Akshayan <makshayan@std.appsc.sab.ac.lk>
2025-01-17 22:35:12 +0530

    Update cli.dart

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,5 +1,9 @@
-import 'package:cli/main.dart' as m;
+import 'package:cli/main.dart' as m; 
+// Imports the main.dart file from the 'cli' package and aliases it as 'm'.
 
 Future<void> main(List<String> arguments) async {
+  // The entry point for the application. Accepts a list of command-line arguments.
+  
   await m.main(arguments);
+  // Calls the main method from the imported 'main.dart' file, passing along the arguments.
 }
