e1365d225a1225d75612407ad60d32d4b60a9050
Lahiru Sampath <113624926+LahiruZaM@users.noreply.github.com>
2025-02-05 08:38:31 +0530

    Minor Changes

--- app/test/mocks.dart
+++
@@ -1,9 +0,0 @@
-import 'package:fileflow/provider/persistence_provider.dart';
-import 'package:mockito/annotations.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-@GenerateNiceMocks([
-  MockSpec<PersistenceService>(),
-  MockSpec<SharedPreferences>(),
-])
-void main() {}
